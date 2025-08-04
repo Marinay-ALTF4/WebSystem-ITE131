e4124b364d4c5f0334e63201ca4cce4742a4921f
Marinay-ALTF4 <adriennemarinay@gmail.com>
2025-08-05 00:08:33 +0800

    Initial commit - CodeIgniter setup

---
+++ app/Views/template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<ul class="nav justify-content-end">';
+        echo '  <li class="nav-item">';
+        echo '    <a class="nav-link active" aria-current="page" href="#">Active</a>';
+        echo '  </li>';
+        echo '  <li class="nav-item">';
+        echo '    <a class="nav-link" href="#">Link</a>';
+        echo '  </li>';
+        echo '  <li class="nav-item">';
+        echo '    <a class="nav-link" href="#">Link</a>';
+        echo '  </li>';
+        echo '  <li class="nav-item">';
+        echo '    <a class="nav-link disabled" aria-disabled="true">Disabled</a>';
+        echo '  </li>';
+        echo '</ul>';
+?>
+    
+</body>
+</html>
