4822d6b25eb654ac37b32c2a26fad5b33b1f43d8
Marinay-ALTF4 <adriennemarinay@gmail.com>
2025-08-21 16:22:52 +0800

    Added database migrations and seeders

---
+++ app/Database/Seeds/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'name'     => 'Admin User',
+                'email'    => 'admin@example.com',
+                'password' => password_hash('admin123', PASSWORD_DEFAULT),
+                'role'     => 'admin',
+            ],
+            [
+                'name'     => 'Justin Nabunturan',
+                'email'    => 'jstnabunturan@gmail.com',
+                'password' => password_hash('student123', PASSWORD_DEFAULT),
+                'role'     => 'student',
+            ],
+            [
+                'name'     => 'Lebron James',
+                'email'    => 'lbrnjames@gmail.com',
+                'password' => password_hash('student123', PASSWORD_DEFAULT),
+                'role'     => 'student',
+            ],
+            [
+                'name'     => 'Lemuel Patotoya',
+                'email'    => 'lmlpatotoya@gmail.com',
+                'password' => password_hash('instructor123', PASSWORD_DEFAULT),
+                'role'     => 'instructor',
+            ],
+        ];
+
+        // Insert multiple rows at once
+        $this->db->table('users')->insertBatch($data);
+    }
+}
